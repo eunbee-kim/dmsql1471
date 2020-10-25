68a30cadd109dd9e6446cfc794afae1b21b12956
dmsql1471 <dmsql1471@gmail.com>
2020-10-25 18:09:42 +0900

    dd

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,68 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``, 인스턴스에서 템플릿은 잘 쓰지 않음 화면을 조합하는애
+    methods: {
+
+    },
+    components: { }, 
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted: function() {
+        console.log("mounted");
+    },
+    updated: function() {
+        console.log("updated");
+    },
+});
+
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    //components: { }, 컴포넌트에서는 data를 function으로 만든다
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
