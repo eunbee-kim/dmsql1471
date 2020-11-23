0de67df260c0589d9db2079c386cb9f214083085
dmsql1471 <dmsql1471@gmail.com>
2020-11-23 10:25:37 +0900

    ff

---
+++ DotNotNote.Database/DotNotNote.Database/dbo/Tables/Points/Points_PointLogs.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Points]
+(
+	[Id] INT Identity(1,1) NOT NULL PRIMARY KEY,
+	[UserId] INT NOT NULL,
+	[Username] NVarChar(25)NULL,
+	[TotalPoint] INT Default(0)
+
+)
+Go
+CREATE TABLE [dbo].[PointLogs]
+(
+	[Id] INT Identity(1,1) NOT NULL PRIMARY KEY,
+	[UserId] INT NOT NULL,
+	[Username] NVarChar(25)NULL,
+	[NowPoint] INT Default(1),
+	[Created]  DatetimeOffset(7) Default(GetDate())
+
+)
+Go
